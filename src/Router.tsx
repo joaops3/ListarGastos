72dfcbce2e38efbd13d4ff59af1f078009268c98
joao pedro <j.psilva3119@gmail.com>
2022-08-21 16:10:19 -0300

    fixed routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Main from "./pages/Main";
 import Editar from "./pages/Editar";
-import History  from "./pages/History";
+import History  from "./pages/Chart";
 
 const Router = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Main></Main>}></Route>
+        <Route path="/ListarGastos" element={<Main></Main>}></Route>
         <Route path="/chart" element={<History></History>}></Route>
         <Route path="/cadastrar" element={<Cadastro></Cadastro>}></Route>
         <Route path="/editar:id" element={<Editar></Editar>}></Route>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
         <header>
             <C.Nav>
             <C.Flex justify="space-between">
-            <h2 ><Link to="/" className='link-nav' style={{color: colors.home}}> HISTORICO</Link></h2>
+            <h2 ><Link to="/ListarGastos" className='link-nav' style={{color: colors.home}}> HISTORICO</Link></h2>
             <h2 ><Link to="/chart" className='link-nav' style={{color: colors.home}}>GRAFICO</Link></h2>
             </C.Flex>
             <Link to="/cadastrar" className="link cadastrar"> <C.cadastrar color={colors.yellow}>CADASTRAR</C.cadastrar></Link>
